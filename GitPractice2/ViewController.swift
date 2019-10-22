4aa8947772f32483413a02caf321f8bb973f3ff7
Fujio Nakatsuka <com.nifty.cbm01799>
2019-10-22 23:35:43 +0900

    Initial Commit

---
+++ GitPractice2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitPractice2
+//
+//  Created by 中塚富士雄 on 2019/10/22.
+//  Copyright © 2019 中塚富士雄. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
